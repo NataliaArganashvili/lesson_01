c506806156105c3663fc7fb968f6e240de47c1b0
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-10-23 02:59:17 +0400

    Update lesson5_task1.py

--- lesson5_task1.py
+++ lesson5_task1.py
@@ -5,10 +5,9 @@ from time import sleep
 driver = webdriver.Chrome()
 driver.get("http://the-internet.herokuapp.com/add_remove_elements/")
 
+for x in range (5):
+    driver.find_element(By.XPATH, "//button[text()='Add Element']").click()
 
-for x in range (6):
-    driver.find_element(By.LINK_TEXT, "Add Element").click()
-
-driver.find_elements(By.LINK_TEXT, "Delete")
+driver.find_elements(By.XPATH, "//button[text()='Delete']")
 
 sleep(5)
