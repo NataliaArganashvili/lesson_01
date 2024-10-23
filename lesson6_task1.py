a798f9247d209707541a66964d0bfc499a7bf90d
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-10-24 00:07:52 +0400

    Update lesson6_task1.py

--- lesson6_task1.py
+++ lesson6_task1.py
@@ -4,11 +4,10 @@ from time import sleep
 
 driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/ajax")
+driver.implicitly_wait(20)
 
 driver.find_element(By.XPATH, "//button[text()='Button Triggering AJAX Request']").click()
 
-sleep(30)
-
-element = driver.find_element(By.CSS_SELECTOR,'#content')
-txt = element.text
+content = driver.find_element(By.CSS_SELECTOR,'#content')
+txt = content.find_element(By.CSS_SELECTOR,'p.bg-success').text
 print(txt)
