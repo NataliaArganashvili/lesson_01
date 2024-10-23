4abb0096cd579bc22fa6959ee12125fe6b881863
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-10-24 03:01:57 +0400

    Update lesson5_task2.py

--- lesson5_task2.py
+++ lesson5_task2.py
@@ -5,6 +5,6 @@ from time import sleep
 driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/dynamicid")
 
-driver.find_element(By.CSS_SELECTOR, '#f2cf7343-1874-9b19-f7a8-b2e378830bb5').click
+driver.find_element(By.CSS_SELECTOR, "button.btn.btn-primary").click()
 
 sleep(5)
