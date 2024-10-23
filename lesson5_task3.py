2f032b0440a28f1474d19b4b85c22c0d1df9abb9
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-10-24 03:02:40 +0400

    Update lesson5_task3.py

--- lesson5_task3.py
+++ lesson5_task3.py
@@ -5,6 +5,6 @@ from time import sleep
 driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/classattr")
 
-driver.find_element(By.CLASS_NAME, ('.class1')).click
+driver.find_element(By.CLASS_NAME, ('btn-primary')).click()
 
 sleep(5)
