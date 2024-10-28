214e587132bc88687ab65bb4195899cee60635ab
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-10-29 03:13:19 +0400

    Update test_shop.py

--- pages_shop/test_shop.py
+++ pages_shop/test_shop.py
@@ -10,24 +10,24 @@ from pages_shop.checkout_page import CheckoutPage
 from pages_shop.overview_page import OverviewPage
 
 def test_shop():
-    main_page = MainPage
+    main_page = MainPage(driver)
     main_page.fill_in_username("standard_user")
-    main_page.fill_in_password
-    main_page.click_login
+    main_page.fill_in_password()
+    main_page.click_login()
     
-    products_page = ProductsPage
-    products_page.click_elements
-    products_page.go_to_cart
+    products_page = ProductsPage(driver)
+    products_page.click_elements()
+    products_page.go_to_cart()
 
-    cart_page = CartPage
-    cart_page.go_to_checkout
+    cart_page = CartPage(driver)
+    cart_page.go_to_checkout()
 
-    checkout_page = CheckoutPage
+    checkout_page = CheckoutPage(driver)
     checkout_page.fill_in_first_name("Natalia")
     checkout_page.fill_in_last_name("Arganashvili")
     checkout_page.fill_in_postal_code("555555")
-    checkout_page.click_continue
+    checkout_page.click_continue()
 
-    overview_page = OverviewPage
-    total_price = overview_page.get_total_price
+    overview_page = OverviewPage(driver)
+    total_price = overview_page.get_total_price()
     assert total_price == 'Total: $58.29'
