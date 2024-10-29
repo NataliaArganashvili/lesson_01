12707a28de0d35706253f3fde7fbb1c83daca527
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-10-29 14:28:20 +0400

    Update test_calculator.py

--- lesson7/test_calculator.py
+++ lesson7/test_calculator.py
@@ -5,8 +5,8 @@ browser = webdriver.Chrome()
 def test_calculator():
     calculator = Calculator(browser)
     calculator.set_delay("45")
-    calculator.click_buttons
-    calculator.waiting
-    as_is = calculator.get_result
+    calculator.click_buttons()
+    calculator.waiting()
+    as_is = calculator.get_result()
 
     assert as_is == "15"
