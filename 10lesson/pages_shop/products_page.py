4b9ea63e319c1556ee200be9dea50c2638eb80bd
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-10-30 20:22:29 +0400

    Add files via upload

---
+++ 10lesson/pages_shop/products_page.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class ProductsPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_elements(self) ->None:
+        """Добавление продуктов в корзину"""
+        self.driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-backpack').click()
+        self.driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-bolt-t-shirt').click()
+        self.driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-onesie').click()
+
+    def go_to_cart(self) ->None:
+        """Переход в корзину"""
+        self.driver.find_element(By.CSS_SELECTOR, 'a.shopping_cart_link').click()
